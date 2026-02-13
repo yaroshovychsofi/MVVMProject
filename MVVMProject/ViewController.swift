13320a9728d49ef48bdd56916000428b6519a266
yaroshovychsofi <sofia.yaroshovych@gmail.com>
2026-02-13 12:22:02 +0200

    Initial Commit

---
+++ MVVMProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMProject
+//
+//  Created by Софія Ярошович on 13.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
